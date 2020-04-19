9c55a43d8848cfb316a913a7d8b8d2284943a6a5
O-mobola <ssalawudeen06@gmail.com>
2020-04-20 00:11:51 +0100

    updated

--- js_task3.js
+++ js_task3.js
@@ -4,44 +4,44 @@
 function stringReplace (number) {
 
   //declare an empty array and a variable
-  let x, y, z, ans, ans1, ans2, ans3, ans4, ans5;
+    let x, y, z;
     x = "yu";
     y = "gi";
     z = "oh";
-  let hold = [];
-  let hold2 = [];
+    let hold = [];
+    let hold2 = [];
   //the count in number
   
-  for(var count=1; count<=number; count++) {
+  for(let count=1; count<number; count++) {
     //check conditions
-    
     if(number%2 === 0) {
-     count = x;
-    //console.log(hold);
+    count = x;
+    a = count;
     }
     else if(number%3 === 0) {
      count= y;
-hold[hold.length] += count;
+     a += count;
     }
     else if(number%5 === 0) {
      count = z;
-hold[hold.length] = count;
+     a += count;
     }
     else if(number%2 ===0 && number%3 === 0) {
-     count= x-y;
-hold[hold.length] = x-y;
+     count= (x+"-"+y);
+     a += count;
     }
-    else if(number[count]%2 ===0 && number[count]%3 === 0 && number[count]%5 === 0) {
-      count= x-y-z;
+    else if(number%2 ===0 && number%3 === 0 && number%5 === 0) {
+      count= (x+"-"+y+"-"+z);
+      a += count;
     }
     else {
-      number;
+      count[number];
     }
-    hold.push(count);
+    hold.push(a);
   }
-  //push count into array hold
-    console.log(hold);
-    return hold;
+  //push count into array hold2
+  
+  console.log(hold);
+  return hold;
 }
-    
-console.log(stringReplace(100));
+console.log(stringReplace(6));
