c87f8e445620c990b0551745c3fc859227ac47fd
O-mobola <ssalawudeen06@gmail.com>
2020-04-20 01:14:58 +0100

    updated

--- js_task3.js
+++ js_task3.js
@@ -4,44 +4,39 @@
 function stringReplace (number) {
 
   //declare an empty array and a variable
-    let x, y, z, a;
+    let x, y, z;
     x = "yu";
     y = "gi";
     z = "oh";
     let hold = [];
-    let hold2 = [];
   //the count in number
   
-  for(let count=1; count<number; count++) {
+  for(let count=0; count <= number; count++) {
     //check conditions
     if(number%2 === 0) {
     count = x;
-    a += count;
     }
     else if(number%3 === 0) {
      count= y;
-     a = count;
     }
     else if(number%5 === 0) {
      count = z;
-     a = count;
     }
     else if(number%2 ===0 && number%3 === 0) {
      count= (x+"-"+y);
-     a = count;
     }
     else if(number%2 ===0 && number%3 === 0 && number%5 === 0) {
       count= (x+"-"+y+"-"+z);
-      a = count;
     }
     else {
-      count[number];
+      count;
     }
-    hold.push(a);
+    //push count into array hold
+    hold.push(count);
   }
-  //push count into array hold2
   
   console.log(hold);
   return hold;
 }
-console.log(stringReplace(6));
+stringReplace(100);
+stringReplace(9);
